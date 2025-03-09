d48e6e27a0fcea39b00fef938416b8bdd7d45e6a
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-09 15:05:52 -0700

    Initialized home page and updated footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./layouts/navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
-    imports: [HeaderComponent, FooterComponent, RouterOutlet],
+    imports: [HeaderComponent, FooterComponent, RouterOutlet, NavbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
