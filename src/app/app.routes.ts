893ef3ee81d1c8b43e2b704bef690ec776d96498
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-09 18:18:01 -0700

    Updated routes for household, business, and property

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PersonPageComponent } from './pages/person-page/person-page.component';
+import { HouseholdPageComponent } from './pages/household-page/household-page.component';
+import { BusinessPageComponent } from './pages/business-page/business-page.component';
+import { PropertyPageComponent } from './pages/property-page/property-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'person', component: PersonPageComponent}
+    {path: 'person', component: PersonPageComponent},
+    {path: 'household', component: HouseholdPageComponent},
+    {path: 'business', component: BusinessPageComponent},
+    {path: 'property', component: PropertyPageComponent}
 ];
