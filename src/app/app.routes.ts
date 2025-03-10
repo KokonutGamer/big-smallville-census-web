251bdd20822028c5b4537ea2f528f3eea367eb07
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-09 18:10:17 -0700

    Initialized person page and updated routing for navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PersonPageComponent } from './pages/person-page/person-page.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'person', component: PersonPageComponent}
 ];
---
+++ src/app/pages/person-page/person-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-page',
+  imports: [],
+  templateUrl: './person-page.component.html',
+  styleUrl: './person-page.component.css'
+})
+export class PersonPageComponent {
+
+}
