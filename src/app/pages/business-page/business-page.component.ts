1d429705e15ca55085fb7d1005df7062880cde03
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-09 18:14:53 -0700

    Generated business, household, and property pages

---
+++ src/app/pages/business-page/business-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-page',
+  imports: [],
+  templateUrl: './business-page.component.html',
+  styleUrl: './business-page.component.css'
+})
+export class BusinessPageComponent {
+
+}
---
+++ src/app/pages/household-page/household-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-household-page',
+  imports: [],
+  templateUrl: './household-page.component.html',
+  styleUrl: './household-page.component.css'
+})
+export class HouseholdPageComponent {
+
+}
