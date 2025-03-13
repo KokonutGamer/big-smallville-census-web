b5f2d5280408b61ac2fa0573501932265e3237c5
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-12 19:25:47 -0700

    Implemented incentives form

--- src/app/pages/person-page/person-page.component.ts
+++ src/app/pages/person-page/person-page.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BackendService } from '../../services/backend.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-person-page',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './person-page.component.html',
   styleUrl: './person-page.component.css'
 })
 export class PersonPageComponent {
+  ssn: string;
 
+  constructor(private backendService: BackendService) {
+    this.ssn = "";
+  }
+
+  submitIncentive(formData: NgForm) {
+    console.log(formData.value)
+  }
 }
