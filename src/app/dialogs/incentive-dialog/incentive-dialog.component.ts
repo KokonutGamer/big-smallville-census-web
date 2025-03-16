44ec91d57d9c0e5ef87a0d643f02bb30a619b859
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-15 18:35:06 -0700

    Fixed incentive dialog errors with mat form field

--- src/app/dialogs/incentive-dialog/incentive-dialog.component.ts
+++ src/app/dialogs/incentive-dialog/incentive-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, input, model } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
@@ -6,11 +6,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { BackendService } from '../../services/backend.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-incentive-dialog',
-  imports: [MatDialogTitle, MatDialogContent, MatFormFieldModule, MatDatepickerModule, MatSelectModule, FormsModule, MatDialogActions, MatButtonModule, MatDialogClose],
+  imports: [MatDialogTitle, MatDialogContent, MatFormFieldModule, MatDatepickerModule, MatSelectModule, FormsModule, MatDialogActions, MatButtonModule, MatDialogClose, MatInputModule],
   templateUrl: './incentive-dialog.component.html',
   styleUrl: './incentive-dialog.component.css'
 })
@@ -19,12 +20,13 @@ export class IncentiveDialogComponent {
 
   readonly dialogRef = inject(MatDialogRef<IncentiveDialogComponent>);
   readonly incentive = signal(0);
-  readonly ssn = signal("");
+
+  readonly ssn = model('');
 
   constructor(private backendService: BackendService) { }
 
-  submitIncentive(formData: NgForm): void {
-
+  submitIncentive(): void {
+    console.log("ssn = ", this.ssn());
   }
 
   onNoClick(): void {
@@ -32,6 +34,6 @@ export class IncentiveDialogComponent {
   }
 
   clearData(): void {
-    this.ssn.set("");
+    // idk what this is for now
   }
 }
