1f5847b363f00ac2c62b337ff82f08494411dab6
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-17 17:33:49 -0700

    Added buttons to dialog prompt for add person

--- src/app/dialogs/add-person-dialog/add-person-dialog.component.ts
+++ src/app/dialogs/add-person-dialog/add-person-dialog.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-add-person-dialog',
-  imports: [MatDialogTitle, MatDialogContent, MatFormFieldModule, MatDatepickerModule, MatSelectModule, MatInputModule],
+  imports: [MatDialogTitle, MatDialogContent, MatFormFieldModule, MatDatepickerModule, MatSelectModule, MatInputModule, MatDialogActions, MatDialogClose, MatButtonModule],
   templateUrl: './add-person-dialog.component.html',
   styleUrl: './add-person-dialog.component.css'
 })
 export class AddPersonDialogComponent {
   today = new Date();
+
+  submitPerson(): void {
+
+  }
 }
