74a733e238d89e8c2e7998ac7624d907d1d06bc0
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-17 19:10:58 -0700

    Added needy parents dialog

---
+++ src/app/dialogs/needy-parents-dialog/needy-parents-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-needy-parents-dialog',
+  imports: [],
+  templateUrl: './needy-parents-dialog.component.html',
+  styleUrl: './needy-parents-dialog.component.css'
+})
+export class NeedyParentsDialogComponent {
+
+}
