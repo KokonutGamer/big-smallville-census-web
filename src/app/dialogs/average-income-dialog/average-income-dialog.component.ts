026ec606e65c2e805854cf3fa81e63798521e4f2
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-18 11:58:40 -0700

    Updated average income dialog

--- src/app/dialogs/average-income-dialog/average-income-dialog.component.ts
+++ src/app/dialogs/average-income-dialog/average-income-dialog.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogActions, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'app-average-income-dialog',
-  imports: [],
+  imports: [MatDialogModule, MatFormFieldModule, FormsModule, MatDialogActions, MatInputModule, MatButtonModule],
   templateUrl: './average-income-dialog.component.html',
   styleUrl: './average-income-dialog.component.css'
 })
 export class AverageIncomeDialogComponent {
+  readonly businessName = model('');
+  readonly averageIncome = model(0);
 
+  constructor(private backendService: BackendService) {}
+
+  submitBusinessName(): void {
+    this.backendService.getAverageIncome(this.businessName()).subscribe((response) => {
+      this.averageIncome.set(response.avgIncome);
+    });
+  }
 }
