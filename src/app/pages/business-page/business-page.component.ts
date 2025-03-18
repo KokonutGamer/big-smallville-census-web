265310c66392e516eb0060fa631ad080ef14c70e
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-18 11:57:52 -0700

    Updated business page with buttons for dialogs

--- src/app/pages/business-page/business-page.component.ts
+++ src/app/pages/business-page/business-page.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { AverageIncomeDialogComponent } from '../../dialogs/average-income-dialog/average-income-dialog.component';
 
 @Component({
   selector: 'app-business-page',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './business-page.component.html',
   styleUrl: './business-page.component.css'
 })
 export class BusinessPageComponent {
+  readonly dialog = inject(MatDialog);
 
+  openAverageIncomeDialog(): void {
+    const buttonElement = document.activeElement as HTMLElement;
+    buttonElement.blur();
+    this.dialog.open(AverageIncomeDialogComponent);
+  }
 }
